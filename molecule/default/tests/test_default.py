1e5b779792f53ce5060864926f8050281613df5c
Anton Ustyuzhanin <au@hellofresh.com>
2018-03-07 09:57:24 +0100

    Add Testinfra tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,3 +12,36 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_sssd_ldap_user(host):
+    user = host.user('test')
+    assert user.uid == 5000
+    assert user.gid == 1
+
+
+def test_sssd_service_state(host):
+    assert host.service('sssd').is_enabled
+    assert host.service('sssd').is_running
+
+
+def test_sshd_service_state(host):
+    assert host.service('sshd').is_enabled
+    assert host.service('sshd').is_running
+
+
+def test_ssh_access(host):
+    host.run_test(
+        '/usr/bin/ssh '
+        '-o StrictHostKeyChecking=no '
+        '-o BatchMode=yes '
+        '-T '
+        '-i /root/.ssh/id_rsa '
+        '-l test '
+        'localhost '
+        'exit'
+    )
+
+
+def test_homedir_created(host):
+    assert host.file('/home/test').is_directory
